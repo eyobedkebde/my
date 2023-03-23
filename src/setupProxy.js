cf3635049fbf1eb59e3fe867eb64a308302fb295
eyobedkebde <eyobed@kemercode>
2023-03-23 20:34:19 +0300

    old like app 7X

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(createProxyMiddleware("/api", {
-    target: "http://zibackend-env.eba-sv8nbbfm.us-east-2.elasticbeanstalk.com",
+    target: "http://zibackend-env.eba-sv8nbbfm.us-east-2.elasticbeanstalk.com/api",
     changeOrigin: true
 }));
 };
