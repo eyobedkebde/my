1d40415a2dc92df15717431a4e53ad893bc23c9a
eyobedkebde <eyobed@kemercode>
2023-03-23 20:09:22 +0300

    removed api form proxy setting

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(createProxyMiddleware("/api", {
-    target: "http://backendserver-env.eba-pmpfkpzk.us-east-1.elasticbeanstalk.com/api",
+    target: "http://zibackend-env.eba-sv8nbbfm.us-east-2.elasticbeanstalk.com",
     changeOrigin: true
 }));
 };
